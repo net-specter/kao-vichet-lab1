7caf062479278725a526470973812278417fca70
net-specter <kaovichet11@gmail.com>
2025-11-08 23:10:47 +0700

    fix test.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, CI/CD !');
+    res.send('Hello, GitHub Actions!');
 });
 
 module.exports = app;
